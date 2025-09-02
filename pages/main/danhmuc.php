15880a576df53ebddb93d74b3b9de64894ab6c37
thiennh <nguyenhaithien2k5@gmail.com>
2025-09-02 15:20:49 +0700

    Giao dien website

---
+++ pages/main/danhmuc.php
@@ -0,0 +1,38 @@
+<h3>Danh mục sản phẩm: Ốp lưng</h3>
+                <ul class="product_list">
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                </ul>
---
+++ pages/main/index.php
@@ -0,0 +1,38 @@
+<h3>Sản phẩm mới nhất</h3>
+                <ul class="product_list">
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                    <li>
+                        <img src="images/tainghe2.png" alt="tai nghe Sony 001">
+                        <p class="title_product">Tên sản phẩm: tai nghe Sony 001</p>
+                        <p class="price_product">Giá: 300000 vnd</p>
+                    </li>
+                </ul>
